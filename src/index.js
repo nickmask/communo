c5e1ea2f2ae23e33520b87d22b12f963908b6874
josephquested <josephquested@gmail.com>
2016-03-17 10:41:13 +1300

    Add basic css plan

--- src/index.js
+++
@@ -1,8 +0,0 @@
-var getData = require('./components/getServerData.js')
-var address = 'http://localhost:3000/api/v1/address'
-var listen = require('./components/listen.js')
-
-// requestData(address)
-//   .then(function (data) {
-
-//   })
