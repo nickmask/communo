df2ad0fe0c036535ace9682b8012c2244d68827b
Nick Maskell <nrmaskell@gmail.com>
2016-03-17 11:33:03 +1300

    test still fail

---
+++ src/components/BlackKey.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default React.createClass({
+  handleOnKeyDown(event) {
+    event.preventDefault()
+    console.log('keypress')
+  },
+  render() {
+    return (
+      <div className={this.props.blackKey} onKeyDown={this.handleOnKeyDown}>
+      </div>
+    )
+  }
+})
