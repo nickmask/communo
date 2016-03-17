49b62f804b8fba027f4293f748aa3d27ead647c0
josephquested <josephquested@gmail.com>
2016-03-17 18:21:01 +1300

    Add experimental socket code

--- server/server.js
+++ server/server.js
@@ -1,9 +1,37 @@
 var express = require('express')
 var path = require('path')
 var compression = require('compression')
+var Primus = require('primus.io')
 
 var app = express()
 app.use(compression())
+
+var primus = new Primus(server, {
+  port: '8080',
+  origins: '*',
+  transformer: 'websockets',
+  parser: 'JSON'
+})
+
+var sparks = []
+primus.on('connection', spark => {
+  sparks.push(spark)
+  console.log('communist connection established')
+
+  spark.on('message', msg => {
+    console.log(msg)
+  })
+
+  spark.on('note', note => {
+    sparks.forEach(sp => {
+      sp.send('note', note)
+    })
+  })
+})
+
+primus.on('disconnection', function (spark) {
+  console.log(spark, ' disconnected')
+})
 
 // serve our static stuff like index.css
 app.use(express.static(path.join(__dirname, 'public')))
---
+++ src/components/SocketTest.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+export default React.createClass({
+
+  componentDidMount: function () {
+    console.log("trying to connect primus...")
+    this.socket = Primus.connect('ws://localhost:8080')
+    this.socket.on('open', function () {
+      this.socket.send('message', { message: 'communist connected' })
+      this.socket.on('note', function (note) {
+        console.log('note', note)
+        this.playSound(note)
+      }.bind(this))
+    }.bind(this))
+  },
+
+  playSound: function (note) {
+    const audio = new Audio(`../../public/audio/${note}.wav`);
+    audio.play()
+  },
+
+  render () {
+    return (
+      <div>
+        <button>socket test</button>
+      </div>
+    )
+  }
+})
